9fcb33801eb5cf0b35714caec210cec659192e27
JOHN SMITH <johnsmithg0051@gmail.com>
2024-01-28 16:12:35 +0530

    test commit

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -44,7 +44,7 @@ export default function NavLinks() {
             )}
           >
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
+              <p className="hidden md:block">{link.name}</p>
           </Link>
         );
       })}
